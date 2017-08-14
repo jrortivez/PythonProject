ba9f26545506fb9875afac6d7451c8b3ae454251
jrortivez <31012721+jrortivez@users.noreply.github.com>
2017-08-14 10:55:58 -0600

    Created story.py
    This is my first project for computer programming. The story is complete/incomplete.
    If the story is incomplete, list things here that still need to be done.
    -need to add comments
    -needs all endings

---
+++ Story.py
@@ -0,0 +1,6 @@
+def begin_story();
+  user_name - input("Please enter your name")
+  print("You're home alone at night, and you hear a pounding at your door.",
+  "what do you do?")
+  print("Enter the number that corresponds to your decision")
+  user_name - int()
